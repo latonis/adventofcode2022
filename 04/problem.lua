127a9248c1762ec1c2b1bdd3179edb3d0b4c6b26
Jacob Latonis <latonis@wisc.edu>
2022-12-04 09:14:56 -0600

    day 4 solved in Lua, efficiently

--- 04/problem.lua
+++ 04/problem.lua
@@ -30,19 +30,67 @@ local function split (inputstr, sep)
     return returnLines
 end
 
+local function rangeCompletelyContains(a, b, c, d)
+    -- check if second in first
+    if (a <= c and c <= b) and (a <= d and d <= b) then
+        return true
+    end
+
+    -- check if first in second
+    if (c <= a and a <= d) and (c <= b and b <= d) then
+        return true
+    end
+end
+
+local function rangeAnyContains(a, b, c, d)
+    -- check if second in first
+    if (a <= c and c <= b) or (a <= d and d <= b) then
+        return true
+    end
+
+    -- check if first in second
+    if (c <= a and a <= d) or (c <= b and b <= d) then
+        return true
+    end
+end
+
 local function solveFirst(input)
     -- do things here to solve problem
+    local count = 0
     for idx, line in pairs(input) do
-        print(line)
+        local entry = split(line, ",")
+        local firstRange = split(entry[1], "-")
+        local secondRange = split(entry[2], "-")
+        local firstStart = tonumber(firstRange[1])
+        local firstEnd = tonumber(firstRange[2])
+        local secondStart = tonumber(secondRange[1])
+        local secondEnd = tonumber(secondRange[2])
+
+        if (rangeCompletelyContains(firstStart, firstEnd, secondStart, secondEnd)) then
+            count = count + 1
+        end
     end
+    print(count)
     -- do things above here to solve the problem
 end
 
 local function solveSecond(input)
     -- do things here to solve problem
+    local count = 0
     for idx, line in pairs(input) do
-        print(line)
+        local entry = split(line, ",")
+        local firstRange = split(entry[1], "-")
+        local secondRange = split(entry[2], "-")
+        local firstStart = tonumber(firstRange[1])
+        local firstEnd = tonumber(firstRange[2])
+        local secondStart = tonumber(secondRange[1])
+        local secondEnd = tonumber(secondRange[2])
+
+        if (rangeAnyContains(firstStart, firstEnd, secondStart, secondEnd)) then
+            count = count + 1
+        end
     end
+    print(count)
     -- do things above here to solve the problem
 end
 
